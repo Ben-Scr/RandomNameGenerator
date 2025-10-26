c9fb7852558f03654da2fcf47e460f2beaa68bed
Ben <benscr.email@gmail.com>
2025-10-26 14:06:41 +0100

    Projektdateien hinzufügen.

---
+++ Playground/Program.cs
@@ -0,0 +1,12 @@
+﻿using RandomNameGenerator;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        NameGenerator nameGenerator = new NameGenerator();
+        Console.WriteLine(nameGenerator.FirstName());
+        Console.WriteLine(nameGenerator.LastName());
+        Console.WriteLine(nameGenerator.FullName(2));
+    }
+}
---
+++ RandomNameGenerator/NameGenerator.cs
@@ -0,0 +1,36 @@
+﻿namespace RandomNameGenerator
+{
+    public class NameGenerator
+    {
+        public static string[] firstNames;
+        public static string[] lastNames;
+
+        private static readonly Random random = new Random();
+
+        public NameGenerator()
+        {
+            if (firstNames == null)
+            {
+                firstNames = File.ReadAllLines("Resources/FirstNames.txt");
+                lastNames = File.ReadAllLines("Resources/LastNames.txt");
+            }
+        }
+
+        public string FullName(int names = 1)
+        {
+            string fullName = "";
+
+            for (int i = 0; i < names; i++)
+                fullName += FirstName() + " ";
+
+            fullName += LastName();
+            return fullName;
+        }
+
+        public string FirstName() => firstNames[random.Next(firstNames.Length)];
+        public string LastName() => lastNames[random.Next(lastNames.Length)];
+
+        public int FirstNamesCount() => firstNames.Length;
+        public int LastNamesCount() => lastNames.Length;
+    }
+}
