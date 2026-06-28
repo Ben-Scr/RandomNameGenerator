d5c6b3b15f758866a0f0c30c32148f0c85638797
Ben Schneider <benscr.email@gmail.com>
2026-06-28 18:12:45 +0200

    Fixed Bugs

--- RandomNameGenerator/Src/NameGenerator.cs
+++ RandomNameGenerator/Src/NameGenerator.cs
@@ -1,57 +1,92 @@
-﻿using System.Reflection;
+using System.Reflection;
+using System.Text;
 
 namespace BenScr.RandomNameGenerator
 {
     public sealed class NameGenerator
     {
-        private static string[] firstNames;
-        private static string[] lastNames;
+        private static readonly string[] firstNames =
+            ReadResourceLines(typeof(NameGenerator).Assembly, "RandomNameGenerator.Resources.FirstNames.txt");
 
-        private static readonly Random random = new Random();
+        private static readonly string[] lastNames =
+            ReadResourceLines(typeof(NameGenerator).Assembly, "RandomNameGenerator.Resources.LastNames.txt");
 
+        /// <summary>Creates a new <see cref="NameGenerator"/>.</summary>
         public NameGenerator()
         {
-            if (firstNames == null)
-            {
-                var assembly = Assembly.GetExecutingAssembly();
-                firstNames = ReadResourceLines(assembly, "RandomNameGenerator.Resources.FirstNames.txt");
-                lastNames = ReadResourceLines(assembly, "RandomNameGenerator.Resources.LastNames.txt");
-            }
         }
 
         private static string[] ReadResourceLines(Assembly assembly, string resourceName)
         {
-            using var stream = assembly.GetManifestResourceStream(resourceName);
+            using var stream = assembly.GetManifestResourceStream(resourceName)
+                ?? throw new InvalidOperationException(
+                    $"Embedded resource '{resourceName}' was not found. " +
+                    $"Available resources: {string.Join(", ", assembly.GetManifestResourceNames())}");
             using var reader = new StreamReader(stream);
-            return reader.ReadToEnd().Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var tokens = reader.ReadToEnd().Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var names = new List<string>(tokens.Length);
+            foreach (var token in tokens)
+            {
+                if (IsValidName(token))
+                    names.Add(token);
+            }
+
+            if (names.Count == 0)
+                throw new InvalidOperationException($"Embedded resource '{resourceName}' contained no usable names.");
+
+            return names.ToArray();
+        }
+
+        private static bool IsValidName(string token)
+        {
+            foreach (char c in token)
+            {
+                if (!char.IsLetter(c) && c != '\'' && c != '-')
+                    return false;
+            }
+
+            return true;
         }
 
         public string FullName(int names = 1)
         {
-            string fullName = "";
+            if (names < 1)
+                throw new ArgumentOutOfRangeException(nameof(names), names, "At least one first name is required.");
 
+            var sb = new StringBuilder();
             for (int i = 0; i < names; i++)
-                fullName += FirstName() + " ";
+                sb.Append(FirstName()).Append(' ');
 
-            fullName += LastName();
-            return fullName;
+            sb.Append(LastName());
+            return sb.ToString();
         }
 
-        public string FirstName() => Capitalize(firstNames[random.Next(firstNames.Length)]);
-        public string LastName() => Capitalize(lastNames[random.Next(lastNames.Length)]);
+        /// <summary>Returns a random, title-cased first name.</summary>
+        public string FirstName() => Capitalize(firstNames[Random.Shared.Next(firstNames.Length)]);
+
+        /// <summary>Returns a random, title-cased last name.</summary>
+        public string LastName() => Capitalize(lastNames[Random.Shared.Next(lastNames.Length)]);
 
         private static string Capitalize(string input)
         {
             if (string.IsNullOrEmpty(input))
                 return input;
 
-            return char.ToUpper(input[0]) + input.Substring(1).ToLower();
+            return char.ToUpperInvariant(input[0]) + input.Substring(1).ToLowerInvariant();
         }
 
+        /// <summary>Returns the number of first names within the array.</summary>
         public static int FirstNamesCount() => firstNames.Length;
+
+        /// <summary>Returns the number of last names within the array.</summary>
         public static int LastNamesCount() => lastNames.Length;
 
-        public static string[] GetFirstNames() => firstNames;
-        public static string[] GetLastNames()  => lastNames;
+        /// <summary>Returns a copy of all first names in the array.</summary>
+        public static string[] GetFirstNames() => (string[])firstNames.Clone();
+
+        /// <summary>Returns a copy of all last names in the array.</summary>
+        public static string[] GetLastNames() => (string[])lastNames.Clone();
     }
 }
